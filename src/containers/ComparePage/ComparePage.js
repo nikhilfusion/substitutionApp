c3ab7295ab96fe2c92dee673107c3128795afc1e
atulpe <atul.sinha@pharmeasy.in>
2017-09-04 10:35:41 +0530

    compare page

--- src/containers/ComparePage/ComparePage.js
+++ src/containers/ComparePage/ComparePage.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { Glyphicon } from 'react-bootstrap';
+import MedicineCard from '../../components/Common/MedicineCard';
+import './ComparePage.css';
 
 class ComparePage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      primaryMedicine: '12345',
+      primaryMedicineId: '12345',
+      medicines: localStorage.getItem('medicines') || [],
     };
   }
   render() {
+    const substitutes = [];
     return (
-      <div />
+      <div className="comparePageContainer">
+        <div className="comparePageHeader">
+          <div className="container">
+            <Link className="homeButton" to="/">
+              <Glyphicon glyph="home" />
+            </Link>
+            <Link className="cartButton" to="/cart">
+              <Glyphicon glyph="shopping-cart" />
+            </Link>
+            Substitutes for XYZ
+          </div>
+        </div>
+        <div className="substituteListContainer">
+          {substitutes.map(subs => <MedicineCard medName={subs} />)}
+        </div>
+      </div>
     );
   }
 }
